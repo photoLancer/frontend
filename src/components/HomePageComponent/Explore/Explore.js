45b2e96c4f61e935b0414980b5e0f1a23a2aee3b
millifail <hwangbird99@gmail.com>
2023-07-11 08:51:03 +0900

    [FE-19] Home navigation component UI 및 maincomntent state로 분기처리

---
+++ src/components/HomePageComponent/Explore/Explore.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Explore() {
+  return <div className='border border-solid border-black'>Explore</div>;
+}
+
+export default Explore;
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './homePage.module.css';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Searchbar from '../components/Searchbar/Searchbar';
+import Explore from '../components/HomePageComponent/Explore/Explore';
+import Bookmark from '../components/HomePageComponent/Bookmark/Bookmark';
+import Following from '../components/HomePageComponent/Following/Following';
+import Notice from '../components/HomePageComponent/Notice/Notice';
 
 function HomePage() {
+  const [homeContent, setHomeContent] = useState(4);
+
+  const exploreHandler = () => {
+    setHomeContent(1);
+  };
+  const bookmarkHandler = () => {
+    setHomeContent(2);
+  };
+  const followingHandler = () => {
+    setHomeContent(3);
+  };
+  const noticeHandler = () => {
+    setHomeContent(4);
+  };
+
   return (
     <>
       <div className={styles.viewport}>
@@ -18,8 +37,42 @@ function HomePage() {
               <div className='searchbar'>
                 <Searchbar />
               </div>
-              <div className='navbar'>navbar</div>
-              <div classname='maincontents'>maincontents</div>
+              <div className='navbar mb-8'>
+                <div className='flex flex-row'>
+                  <button
+                    className={styles.navbar_button}
+                    onClick={exploreHandler}
+                  >
+                    Explore
+                  </button>
+                  <button
+                    className={styles.navbar_button}
+                    onClick={bookmarkHandler}
+                  >
+                    Bookmark
+                  </button>
+                  <button
+                    className={styles.navbar_button}
+                    onClick={followingHandler}
+                  >
+                    Following
+                  </button>
+                  <button
+                    className={styles.navbar_button}
+                    onClick={noticeHandler}
+                  >
+                    Notice
+                  </button>
+                </div>
+                <hr className={styles.navbar_hr} />
+              </div>
+
+              <div className='maincontents border border-solid border-red-500'>
+                {homeContent === 1 ? <Explore /> : ''}
+                {homeContent === 2 ? <Bookmark /> : ''}
+                {homeContent === 3 ? <Following /> : ''}
+                {homeContent === 4 ? <Notice /> : ''}
+              </div>
             </div>
           </div>
         </div>
