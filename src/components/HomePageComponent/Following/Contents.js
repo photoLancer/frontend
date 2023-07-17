2c9e9101632fb0ce560ba5a1f2d11b2e161bd886
hongsy521 <seoyeong0521@naver.com>
2023-07-17 18:17:15 +0900

    [FE-11] following 수정

--- src/components/HomePageComponent/Following/Contents.js
+++ src/components/HomePageComponent/Following/Contents.js
@@ -4,18 +4,18 @@ import styles from './contents.module.css';
 function Contents(){
     return(
         <>
-        <div className={styles.frame}>
+      
         <div className={styles.headframe}>
           <img className={styles.smallimg}></img>
-          <div className={styles.nickname}>??</div>
-          <div className={styles.icon}>&gt</div>
+          <p className={styles.nickname}>?</p>
+          <p className={styles.icon}>-</p>
         </div>
         <div className={styles.contentsframe}>
           <img className={styles.content}></img>
           <img className={styles.content}></img>
           <img className={styles.content}></img>
         </div>
-        </div>
+       
         </>
     );
 }
