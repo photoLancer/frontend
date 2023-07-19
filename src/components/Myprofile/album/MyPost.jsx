8fda1d705eec4bf6dcab1a93480d735a92de614e
hongsy521 <seoyeong0521@naver.com>
2023-07-20 03:04:36 +0900

    [FE-17] 분기처리 후 CSS 작업중

---
+++ src/components/Myprofile/album/MyPost.jsx
@@ -0,0 +1,10 @@
+import styles from './mypost.module.css';
+
+const MyPost=()=>{
+    return(
+        <>
+        digdh
+        </>
+    );
+}
+export default MyPost;
--- src/components/Myprofile/album/UserPhoto.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './userphoto.module.css';
-
-const UserPhoto=()=>{
-    return(
-        <>
-        </>
-    );
-}
-export default UserPhoto;
