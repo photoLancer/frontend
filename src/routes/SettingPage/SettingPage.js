c384cfe4c78fec07e4f2a2ef6609a92ba335ce23
hongsy521 <seoyeong0521@naver.com>
2023-07-21 01:38:17 +0900

    [FE-25] CSS 수정중

--- src/routes/SettingPage/SettingPage.js
+++ src/routes/SettingPage/SettingPage.js
@@ -2,8 +2,19 @@ import React, { useState } from 'react';
 import styles from './setting.module.css';
 import Header from '../../components/Header/Header';
 import Sidebar from '../../components/Sidebar/Sidebar';
+import Payment from '../../components/SettingPage/Payment';
+import Account from '../../components/SettingPage/Account';
 
 const SettingPage=()=>{
+    const [settingContent,setSettingContent]=useState(1);
+
+    const accountHandler=()=>{
+        setSettingContent(1);
+    };
+    const paymentHandler=()=>{
+        setSettingContent(2);
+    };
+
     return(
         <>
         <div className={styles.viewport}>
@@ -14,7 +25,17 @@ const SettingPage=()=>{
               <Sidebar current_page={2} />
             </div>
             <div className='main basis-3/4 border border-solid border-black'>
-
+            <p className={styles.head}>설정</p>
+            <div className='flex flex-row'>
+                <button className={styles.navbar_button} 
+                onClick={accountHandler}>계정 정보</button>
+                <button className={styles.navbar_button}
+                onClick={paymentHandler}>결제 정보</button>
+            </div>
+            <div className='maincontents border border-solid border-red-500'>
+                {settingContent === 1 ? <Account /> : ''}
+                {settingContent === 2 ? <Payment /> : ''}
+              </div>
 
             </div>
             </div>
