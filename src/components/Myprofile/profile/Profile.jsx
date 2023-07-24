2ee88c4886eb94b6ebda3e489fec93add5cb4a77
millifail <hwangbird99@gmail.com>
2023-07-24 12:59:49 +0900

    프로필 오류 수정

--- src/components/Myprofile/profile/Profile.jsx
+++ src/components/Myprofile/profile/Profile.jsx
@@ -1,32 +1,39 @@
-import React,{useState} from 'react';
-import UserInfo from "./UserInfo";
-import UserLevel from "./UserLevel";
-import UserPoint from "./UserPoint";
+import React, { useState } from 'react';
+import UserInfo from './UserInfo';
+import UserLevel from './UserLevel';
+import UserPoint from './UserPoint';
 import EditProfile from './edit/EditProfile';
 import styles from './profile.module.css';
 
-const Profile=()=>{
-  const [isEditing,setIsEditing]=useState(false);
+const Profile = () => {
+  const [isEditing, setIsEditing] = useState(false);
 
-  const editprofileHandler=()=>{
-      setIsEditing(true);
+  const editprofileHandler = () => {
+    setIsEditing(true);
   };
 
-    return(
-        <>
-        {isEditing ? 
-        (<div className='main basis-3/4 border border-solid border-black'>
-        <EditProfile /></div>)
-        :(<div className='main basis-3/4 border border-solid border-black'>
-              <UserInfo>
-              <button className={styles.editprofilebtn} onClick={editprofileHandler}>Edit Profile</button>
-              </UserInfo>
-              <div className={styles.smallboxing}>
-                <UserLevel />
-                <UserPoint />
-              </div>
-            </div>)}
-        </>
-    );
-}
+  return (
+    <>
+      {isEditing ? (
+        <div className='main basis-3/4 border border-solid border-black'>
+          <EditProfile />
+        </div>
+      ) : (
+        <div className='main basis-3/4 border border-solid border-black'>
+          <button
+            className={styles.editprofilebtn}
+            onClick={editprofileHandler}
+          >
+            Edit Profile
+          </button>
+          <UserInfo></UserInfo>
+          <div className={styles.smallboxing}>
+            <UserLevel />
+            <UserPoint />
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
 export default Profile;
