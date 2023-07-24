a3726cb83c239b54d23ee663063af05790f56038
hongsy521 <seoyeong0521@naver.com>
2023-07-24 18:45:03 +0900

    [FE-37] 사진올리기 UI

--- src/components/UploadPhoto/UploadPhoto.js
+++ src/components/UploadPhoto/UploadPhoto.js
@@ -17,9 +17,11 @@ function UploadPhoto() {
           <div className={styles.uploadwrap}>
           <p className={styles.head}>사진 올리기</p>
           <form action='/target' className={styles.dropzone} id='myDropzone'>
+            <div className={styles.innerwrap}>
             <p className={styles.text1}>파일을 이곳에 끌어 놓아주세요</p><br />
             <p className={styles.text2}>한 번에 1장까지만 업로드 가능</p>
             <button className={styles.selectbtn}>컴퓨터에서 선택</button>
+            </div>
           </form>
           <button className={styles.nextbtn}>다음</button>
           </div>
