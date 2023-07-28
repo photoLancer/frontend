efcb67b328daa566880aa8d9513b768865ef0626
hongsy521 <seoyeong0521@naver.com>
2023-07-28 23:53:01 +0900

    [FE-29] 비밀번호 변경 버튼 handler 설정

--- src/components/SettingPage/Account.jsx
+++ src/components/SettingPage/Account.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import styles from './account.module.css';
 
 const Account=()=>{
+    const [isChanging,setIsChanging]=useState(false);
+    const [isConfirming,setIsConfirming]=useState(false);
+
+    const changepwHandler=()=>{
+        setIsChanging(true);
+        setIsConfirming(false);
+    };
+    const confirmpwHandler=()=>{
+        setIsConfirming(true);
+        setIsChanging(false);
+    };
+
     return(
     <>
     <div className={styles.bigbox}>
@@ -27,15 +40,33 @@ const Account=()=>{
                 </div>
             </div>
         </div>
-        <div className={styles.middlebox2}>
-            <p className={styles.headtext}>암호</p>
+        <div className={styles.middlebox2}> 
             <div className={styles.content2}>
+        {isChanging &&(
+           <>
+           <div className={styles.boxing0}>
+           <div className={styles.boxing1}>
+                    <p className={styles.pwtext}>새 비밀번호</p><br/>
+                    <input type='password' className={styles.inputbox}></input>
+                </div>
+                <div className={styles.boxing2}>
+                    <p className={styles.pwtext}>새 비밀번호 확인</p><br/>
+                    <input type='password' className={styles.inputbox}></input>
+                </div>
+                </div>
+                <div className={styles.changepw}><button className={styles.changepwbtn} onClick={confirmpwHandler}>변경하기</button></div>
+           </>
+        )}
+        {isConfirming &&(
+        <>
                 <div className={styles.smallbox2}>
                     <p className={styles.pwtext}>현재 이용 비밀번호</p>
-                    <input type='password' className={styles.pwbox}></input>
+                    <div className={styles.pwbox}>~~~</div>
                 </div>
-                <div className={styles.changepw}><button className={styles.changepwbtn}>변경하기</button></div>
-            </div>
+                <div className={styles.changepw}><button className={styles.changepwbtn} onClick={changepwHandler}>변경하기</button></div>
+         </>
+        )}
+        </div>
         </div>
     </div>
     </>
