2d4970c80ebbeebc15fa445760e3970ab00f2681
hongsy521 <seoyeong0521@naver.com>
2023-07-28 21:22:59 +0900

    [FE-28] css 약간 수정

--- src/components/Myprofile/profile/edit/EditProfile.jsx
+++ src/components/Myprofile/profile/edit/EditProfile.jsx
@@ -3,21 +3,29 @@ import styles from './editprofile.module.css';
 import Profile from '../Profile';
 
 const EditProfile=()=>{
-    const [inputValue,setInputValue]=useState('');
+    const [inputValue, setInputValue] = useState('');
 
-    const handleChange=(event)=>{
+    const HandleChange=(event)=>{
         setInputValue(event.target.value);
     };
 
     const [applyData,setApplyData]=useState(false);
+    const [cancelData,setCancelData]=useState(false);
 
     const applyHandler=()=>{
+        setInputValue(inputValue);
         setApplyData(true);
+        setCancelData(false);
+    };
+    const cancelHandler=()=>{
+        setInputValue('');
+        setApplyData(false);
+        setCancelData(true);
     };
 
     return(
         <>
-        {applyData ? (
+        {applyData || cancelData ? (
         <Profile />
         ) : (
         <>
@@ -30,21 +38,21 @@ const EditProfile=()=>{
             <div className={styles.editinfo}>
                 <div className={styles.nicknamewrap}>
                 <p className={styles.nicknamehead}>닉네임</p>
-                <input type='text' className={styles.nickname} placeholder='제목을 입력해주세요'value={inputValue} onChange={handleChange}/>
+                <input type='text' className={styles.nickname} placeholder='제목을 입력해주세요'value={inputValue} onChange={HandleChange}/>
                 </div>
                 <div className={styles.infotextwrap}>
                 <p className={styles.infohead}>소개</p>
-                <input type='text' className={styles.infotext} placeholder='소개를 입력해주세요' value={inputValue} onChange={handleChange} />
+                <input type='text' className={styles.infotext} placeholder='소개를 입력해주세요' value={inputValue} onChange={HandleChange} />
                 </div>
             </div>
             <div className={styles.bookmark}>
                 <div className={styles.bookmarkwrap}>
                 <p className={styles.bookmarkhead}>북마크 설정</p>
-                <input type='text' className={styles.search} placeholder='#Trip' value={inputValue} onChange={handleChange}/>
+                <div className={styles.searchwrap}><input type='text' className={styles.search} placeholder='#Trip' value={inputValue} onChange={HandleChange}/><button className={styles.add}>+</button></div>
                 <div className={styles.tagbox}>
                     <div className={styles.tagwrap}>
-                    <span className={styles.tag}>#Trip</span>
-                    <span className={styles.tag}>#food</span>
+                    <span className={styles.tag}>#Trip<button className={styles.delete}>x</button></span>
+                    <span className={styles.tag}>#food<button className={styles.delete}>x</button></span>
                     
                     </div>
                 </div>
@@ -54,7 +62,7 @@ const EditProfile=()=>{
         </div>
         <div className={styles.footer}>
             <button className={styles.apply} onClick={applyHandler}>적용</button>
-            <button className={styles.cancel } onClick={applyHandler}>취소</button>
+            <button className={styles.cancel } onClick={cancelHandler}>취소</button>
         </div>
         </>
         )}
