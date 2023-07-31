c16dd7d0df0af8740eb3b22fa2c9477a371825df
hongsy521 <seoyeong0521@naver.com>
2023-07-31 23:30:13 +0900

    [FE-37] 사진 올리기 세번째 UI

--- src/components/UploadPhoto/UploadPhoto.js
+++ src/components/UploadPhoto/UploadPhoto.js
@@ -2,31 +2,46 @@ import React, { useContext, useState } from 'react';
 import styles from './uploadphoto.module.css';
 import { UploadDispatchContext } from '../../App';
 import Uploading from './Uploading';
+import Uploaded from './Uploaded';
 
 function UploadPhoto() {
   const uploadDispatch = useContext(UploadDispatchContext);
-  const screenClickHandler = () => {
+  /*const screenClickHandler = () => {
     uploadDispatch({ type: 'SCREEN_CLICK' });
   };
   const uploadScreenClickHandler = (e) => {
     e.stopPropagation();
-  };
+  };*/
   
-  const [uploadPhoto1,setUploadPhoto1]=useState(false);
-  const [uploadPhoto2,setUploadPhoto2]=useState(true);
+  const [uploadPhoto1,setUploadPhoto1]=useState(true);
+  const [uploadPhoto2,setUploadPhoto2]=useState(false);
+  const [uploadPhoto3,setUploadPhoto3]=useState(false);
 
   const nextStepHandler1=()=>{
     setUploadPhoto1(false);
     setUploadPhoto2(true);
+    setUploadPhoto3(false);
   };
   const nextStepHandler2=()=>{
     setUploadPhoto1(false);
-    setUploadPhoto2(true);
+    setUploadPhoto2(false);
+    setUploadPhoto3(true);
   };
+  const cancelHandler=()=>{
 
+  }
+  const backHandler=()=>{
+    setUploadPhoto1(false);
+    setUploadPhoto2(true);
+    setUploadPhoto3(false);
+  };
+  const finishHandler=()=>{
+
+  }
+  
   return (   
     <>
-      <div className={styles.screen} onClick={screenClickHandler}>
+      <div className={styles.screen}>
       <div className={styles.uploadScreen}>
           {uploadPhoto1&&(
             <>
@@ -49,7 +64,19 @@ function UploadPhoto() {
           <>
             <Uploading />
             <button className={styles.nextbtn2} onClick={nextStepHandler2}>다음</button>
-          </>)}
+            
+          </>
+          )}
+           {uploadPhoto3&&(
+          <>
+            <Uploaded />
+            <div className={styles.btnwrap}>
+            <button className={styles.cancelbtn} onClick={cancelHandler}>취소</button>
+            <button className={styles.backbtn} onClick={backHandler}>뒤로 가기</button>
+            <button className={styles.finishbtn} onClick={finishHandler}>게시글 업로드</button>
+            </div>
+          </>
+          )}
         
           </div>
       </div>
