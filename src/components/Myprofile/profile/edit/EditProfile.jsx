393407acd786adc13c4cdcb4ac731717c3a6d7a5
hongsy521 <seoyeong0521@naver.com>
2023-08-01 22:26:55 +0900

    [FE-37] 북마크 등록/삭제 기능

--- src/components/Myprofile/profile/edit/EditProfile.jsx
+++ src/components/Myprofile/profile/edit/EditProfile.jsx
@@ -23,6 +23,24 @@ const EditProfile=()=>{
         setCancelData(true);
     };
 
+    const [tagText, setTagText] = useState('');
+    const [tags, setTags] = useState([]);
+  
+    const handleInputChange = (event) => {
+      setTagText(event.target.value);
+    };
+  
+    const handleAddTag = () => {
+      if (tagText.trim() !== '') {
+        setTags([...tags, tagText.trim()]);
+        setTagText('');
+      }
+    };
+  
+    const handleDeleteTag = (tag) => {
+      setTags(tags.filter((t) => t !== tag));
+    };
+
     return(
         <>
         {applyData || cancelData ? (
@@ -36,6 +54,7 @@ const EditProfile=()=>{
                 <button className={styles.editbtn}>사진 변경</button>
             </div>
             <div className={styles.editinfo}>
+                <div className={styles.editinfowrap}>
                 <div className={styles.nicknamewrap}>
                 <p className={styles.nicknamehead}>닉네임</p><br/>
                 <input type='text' className={styles.nickname} placeholder='제목을 입력해주세요'value={inputValue} onChange={HandleChange}/>
@@ -44,19 +63,42 @@ const EditProfile=()=>{
                 <p className={styles.infohead}>소개</p><br/>
                 <input type='text' className={styles.infotext} placeholder='소개를 입력해주세요' value={inputValue} onChange={HandleChange} />
                 </div>
+                </div>
             </div>
-            <div className={styles.bookmark}>
-                <div className={styles.bookmarkwrap}>
-                <p className={styles.bookmarkhead}>북마크 설정</p>
-                <div className={styles.searchwrap}><input type='text' className={styles.search} placeholder='#Trip' value={inputValue} onChange={HandleChange}/><button className={styles.add}>+</button></div>
-                <div className={styles.tagbox}>
-                    <div className={styles.tagwrap}>
-                    <span className={styles.tag}>#Trip<button className={styles.delete}>x</button></span>
-                    <span className={styles.tag}>#food<button className={styles.delete}>x</button></span>
-                    
-                    </div>
+            <div className={styles.thirdbox}>
+            <div className={styles.bookmarkwrap}>
+            <p className={styles.bookmarkhead}>북마크 설정</p>
+            <div className={styles.searchwrap}>
+                <input
+                type='text'
+                className={styles.search}
+                placeholder='#Trip'
+                value={tagText}
+                onChange={handleInputChange}
+                />
+                <button className={styles.add} onClick={handleAddTag}>+</button></div>
+            <div className={styles.tagbox}>
+                <div className={styles.tagwrap}>
+                {tags.map((tag, index) => (<span className={styles.tag} key={index}>
+                    {tag}
+                    <button
+                        className={styles.delete}
+                        onClick={() => handleDeleteTag(tag)}
+                    >x</button>
+                    </span>
+                ))}
                 </div>
-                </div>
+            </div>
+            </div>
+            <div className={styles.purpose}>
+                <p className={styles.texthead}>이용 목적</p>
+                <form className={styles.selection}>
+                    <select name='purpse' className={styles.selectbox}>
+                        <option className={styles.optionvalue}>취미</option>
+                        <option className={styles.optionvalue}>비즈니스</option>
+                    </select>
+                </form>
+            </div>
             </div>
             
         </div>
