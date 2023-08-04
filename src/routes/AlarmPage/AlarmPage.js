e055742dd356693f73623206ac7b7dc6bc8010f0
millifail <hwangbird99@gmail.com>
2023-08-04 18:16:50 +0900

    [FE-43] 알림 페이지 틀 만들기

---
+++ src/routes/AlarmPage/AlarmPage.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import styles from './alarmpage.module.css';
+import Header from '../../components/Header/Header';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import Searchbar from '../../components/Searchbar/Searchbar';
+import Total from './AlarmPageComponent/Total/Total';
+import Sell from './AlarmPageComponent/Sell/Sell';
+import Like from './AlarmPageComponent/Like/Like';
+import System from './AlarmPageComponent/System/System';
+
+function AlarmPage() {
+  const [selectedButton, setSelectedButton] = useState(1);
+  const totalHandler = () => {
+    setSelectedButton(1);
+  };
+  const sellHandler = () => {
+    setSelectedButton(2);
+  };
+  const likeHandler = () => {
+    setSelectedButton(3);
+  };
+  const systemHandler = () => {
+    setSelectedButton(4);
+  };
+  return (
+    <>
+      <div className={styles.viewport}>
+        <div className={styles.contents}>
+          <Header />
+          <div className='flex flex-row'>
+            <div className='sidebar basis-1/4 flex flex-row justify-center border border-solid border-black'>
+              <Sidebar current_page={1} />
+            </div>
+            <div className='main basis-3/4 border border-solid border-black'>
+              <div className='searchbar'>
+                <Searchbar />
+              </div>
+              <div className='navbar mb-8'>
+                <div className='flex flex-row'>
+                  <button
+                    className={`${styles.navbar_button} ${
+                      selectedButton === 1 ? styles.selected : ''
+                    }`}
+                    onClick={totalHandler}
+                  >
+                    전체
+                  </button>
+                  <button
+                    className={`${styles.navbar_button} ${
+                      selectedButton === 2 ? styles.selected : ''
+                    }`}
+                    onClick={sellHandler}
+                  >
+                    구매,판매
+                  </button>
+                  <button
+                    className={`${styles.navbar_button} ${
+                      selectedButton === 3 ? styles.selected : ''
+                    }`}
+                    onClick={likeHandler}
+                  >
+                    공감
+                  </button>
+                  <button
+                    className={`${styles.navbar_button} ${
+                      selectedButton === 4 ? styles.selected : ''
+                    }`}
+                    onClick={systemHandler}
+                  >
+                    시스템
+                  </button>
+                </div>
+                <hr className={styles.navbar_hr} />
+              </div>
+              <div className='maincontents border border-solid border-red-500'>
+                {selectedButton === 1 ? <Total /> : ''}
+                {selectedButton === 2 ? <Sell /> : ''}
+                {selectedButton === 3 ? <Like /> : ''}
+                {selectedButton === 4 ? <System /> : ''}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AlarmPage;
