380eb1348551458b802ac1d64e8c54135c610dd3
hongsy521 <seoyeong0521@naver.com>
2023-08-14 14:30:36 +0900

    [FE-56] 팔로잉

--- src/components/HomePageComponent/Following/Following.js
+++ src/components/HomePageComponent/Following/Following.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './following.module.css';
-import Contents from './Contents';
+import { useSelector } from 'react-redux';
+import { Row } from 'antd';
+import PhotoCard from '../PhotoCard/PhotoCard';
+import axios from 'axios';
 
 function Following() {
+  const [followingInfo,setFollowingInfo]=useState(null);
+  const userState =useSelector((state)=>state.user);
+
+  useEffect(()=>{
+    const fetchFollowing = async()=>{
+      const response = axios.get('http://photolancer.shop/following/users',{
+        headers: {
+          Authorization: userState.jwt,
+        },
+      });
+      setFollowingInfo(response.data);
+    };
+    fetchFollowing();
+  },[]);
+
   return(
     <>
-    <Contents />
-    <Contents />
-    <Contents />
+   <div>안녕</div>
+        <Row gutter={[24, 24]}>
+              {followingInfo &&
+                followingInfo.map((photo, index) => (
+                  <React.Fragment key={index}>
+                    <img className={styles.smallimg}></img>
+                    <p className={styles.nickname}>{nickname+'>'}</p>
+                    <PhotoCard id={photo.postId} image={photo.thumbNailUri} />
+                  </React.Fragment>
+                ))}
+            </Row>
+        
     </>
   );
 }
