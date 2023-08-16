1bd0b2e8fc09dad8da47c1a59a37248b4f5c2d4f
millifail <hwangbird99@gmail.com>
2023-08-16 17:15:22 +0900

    [FE-67] isloggedIn에 따른 페이지 접근 관리

--- src/routes/MembershipPage/MembershipPage.js
+++ src/routes/MembershipPage/MembershipPage.js
@@ -5,18 +5,18 @@ import { Link, useNavigate } from 'react-router-dom';
 function MembershipPage() {
   const navigate = useNavigate();
   const loginHandler = () => {
-    navigate('./CreateAccount');
+    navigate('/CreateAccount');
   };
   return (
     <div className={styles.MembershipPage}>
-      <img className={styles.L_img} src={Login} alt="login_camera" />
+      <img className={styles.L_img} src={Login} alt='login_camera' />
       <div className={styles.create_account}>
         <p>Create Account</p>
         <div>카카오로 회원가입</div>
         <div>네이버로 회원가입</div>
         <div>구글로 회원가입</div>
         <div onClick={loginHandler}>
-          <Link to="./CreateAccount">일반 아이디로 회원가입</Link>
+          <Link to='./CreateAccount'>일반 아이디로 회원가입</Link>
         </div>
       </div>
     </div>
